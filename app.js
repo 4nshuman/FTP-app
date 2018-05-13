7e8b9c08600567fec7ea4ceb09b51e3a509bbba2
heisen04 <32158606+heisen04@users.noreply.github.com>
2018-05-13 14:19:32 +0530

    updated app.js
    added the middleware for javascripts

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.set('view engine','ejs')
 //set middleware to access static content
 app.use('/imgs', express.static(__dirname+'/assets/imgs'))
 app.use('/css', express.static(__dirname+'/assets/css'))
+app.use('/js', express.static(__dirname+'/assets/js'))
 
 //define routing
 app.get('/', function(req,res){
